3728014d6a268ed175aeae86b04ada86eb308c5f
suryafotedar <suryafotedar@gmail.com>
2019-04-22 12:27:30 -0500

    Completed till usage of props

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // TODO
+const GroceryListItems = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+);
+
+let GroceryList = () => (
+  <div>
+    <h2>GroceryList</h2>
+    <GroceryListItems items={['Apples', 'Bananas']}/>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
